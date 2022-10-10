e5ebe9d2d615e1a63801ad2d7459c245ec3185ad
Scott Leggett <scott@sl.id.au>
2022-10-10 17:43:50 +0800

    feat: ssh token generation functionality

---
+++ internal/sshtoken/authhandler.go
@@ -0,0 +1,86 @@
+package sshtoken
+
+import (
+	"context"
+	"errors"
+
+	"github.com/gliderlabs/ssh"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/uselagoon/ssh-portal/internal/lagoondb"
+	"go.uber.org/zap"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+type ctxKey int
+
+const (
+	userUUID ctxKey = iota
+)
+
+// LagoonDBService provides methods for querying the Lagoon API DB.
+type LagoonDBService interface {
+	UserBySSHFingerprint(context.Context, string) (*lagoondb.User, error)
+}
+
+var (
+	authnAttemptsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "authentication_attempts_total",
+		Help: "The total number of authentication attempts",
+	})
+	authnSuccessTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "authentication_success_total",
+		Help: "The total number of successful authentication attempts",
+	})
+	authnAttemptsNonLagoonUser = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "authentication_attempts_non_lagoon_user",
+		Help: "The total number of failed authentication attempts with a user other than lagoon",
+	})
+)
+
+// pubKeyAuth returns a ssh.PublicKeyHandler which accepts any key which
+// matches a user, and the associated user UUID to the ssh context.
+func pubKeyAuth(log *zap.Logger, l LagoonDBService) ssh.PublicKeyHandler {
+	return func(ctx ssh.Context, key ssh.PublicKey) bool {
+		authnAttemptsTotal.Inc()
+		// parse SSH public key
+		pubKey, err := gossh.ParsePublicKey(key.Marshal())
+		if err != nil {
+			log.Warn("couldn't parse SSH public key",
+				zap.String("sessionID", ctx.SessionID()),
+				zap.Error(err))
+			return false
+		}
+		// validate user string
+		if ctx.User() != "lagoon" {
+			authnAttemptsNonLagoonUser.Inc()
+			log.Debug(`invalid user: only "lagoon" is supported`,
+				zap.String("sessionID", ctx.SessionID()),
+				zap.String("user", ctx.User()))
+			return false
+		}
+		// identify Lagoon user by ssh key fingerprint
+		fingerprint := gossh.FingerprintSHA256(pubKey)
+		user, err := l.UserBySSHFingerprint(ctx, fingerprint)
+		if err != nil {
+			if errors.Is(err, lagoondb.ErrNoResult) {
+				log.Debug("unknown SSH Fingerprint",
+					zap.String("sessionID", ctx.SessionID()))
+			} else {
+				log.Warn("couldn't query for user by SSH key fingerprint",
+					zap.String("sessionID", ctx.SessionID()),
+					zap.Error(err))
+			}
+			return false
+		}
+		// The SSH key fingerprint was in the database so authentication was
+		// successful. Inject the user UUID into the context so it can be used in
+		// the session handler.
+		authnSuccessTotal.Inc()
+		ctx.SetValue(userUUID, user.UUID)
+		log.Info("authentication successful",
+			zap.String("sessionID", ctx.SessionID()),
+			zap.String("fingerprint", fingerprint))
+		return true
+	}
+}
---
+++ internal/sshtoken/serve.go
@@ -0,0 +1,45 @@
+// Package sshtoken is the SSH token generation component of Lagoon.
+package sshtoken
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/gliderlabs/ssh"
+	"github.com/uselagoon/ssh-portal/internal/keycloak"
+	"github.com/uselagoon/ssh-portal/internal/lagoondb"
+	"go.uber.org/zap"
+)
+
+// give an 8 second deadline to shut down cleanly.
+const shutdownTimeout = 8 * time.Second
+
+// Serve contains the main ssh session logic
+func Serve(ctx context.Context, log *zap.Logger, l net.Listener,
+	ldb *lagoondb.Client, k *keycloak.Client, hostKeys [][]byte) error {
+	srv := ssh.Server{
+		Handler:          sessionHandler(log, k),
+		PublicKeyHandler: pubKeyAuth(log, ldb),
+	}
+	for _, hk := range hostKeys {
+		if err := srv.SetOption(ssh.HostKeyPEM(hk)); err != nil {
+			return fmt.Errorf("invalid host key: %v", err)
+		}
+	}
+	go func() {
+		// As soon as the top level context is cancelled, shut down the server.
+		<-ctx.Done()
+		shutCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+		defer cancel()
+		if err := srv.Shutdown(shutCtx); err != nil {
+			log.Warn("couldn't shutdown cleanly", zap.Error(err))
+		}
+	}()
+	if err := srv.Serve(l); !errors.Is(ssh.ErrServerClosed, err) {
+		return err
+	}
+	return nil
+}
