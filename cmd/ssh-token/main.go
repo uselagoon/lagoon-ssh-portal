aea28c0eacd2dea9f8d0f2d373b4fb8d9ae749c9
Scott Leggett <scott@sl.id.au>
2022-11-10 23:32:11 +0800

    feat: add ssh-token command

---
+++ cmd/ssh-token/main.go
@@ -0,0 +1,32 @@
+// Package main is the executable ssh-token service.
+package main
+
+import (
+	"github.com/alecthomas/kong"
+	"go.uber.org/zap"
+)
+
+// CLI represents the command-line interface.
+type CLI struct {
+	Debug   bool       `kong:"env='DEBUG',help='Enable debug logging'"`
+	Serve   ServeCmd   `kong:"cmd,default=1,help='(default) Serve ssh-token requests'"`
+	Version VersionCmd `kong:"cmd,help='Print version information'"`
+}
+
+func main() {
+	// parse CLI config
+	cli := CLI{}
+	kctx := kong.Parse(&cli,
+		kong.UsageOnError(),
+	)
+	// init logger
+	var log *zap.Logger
+	if cli.Debug {
+		log = zap.Must(zap.NewDevelopment(zap.AddStacktrace(zap.ErrorLevel)))
+	} else {
+		log = zap.Must(zap.NewProduction())
+	}
+	defer log.Sync() //nolint:errcheck
+	// execute CLI
+	kctx.FatalIfErrorf(kctx.Run(log))
+}
