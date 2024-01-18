cbff9ab0fa528314ed4d5a461d6911020cbbab5c
Scott Leggett <scott@sl.id.au>
2024-01-18 21:45:27 +0800

    chore: simplify test helpers

--- internal/sshserver/helper_test.go
+++ internal/sshserver/helper_test.go
@@ -2,11 +2,7 @@ package sshserver
 
 // ParseConnectionParams exposes the private parseConnectionParams for testing
 // only.
-func ParseConnectionParams(args []string) (string, string, string, []string) {
-	return parseConnectionParams(args)
-}
+var ParseConnectionParams = parseConnectionParams
 
 // ParseLogsArg exposes the private parseLogsArg for testing only.
-func ParseLogsArg(service, logs string, args []string) (bool, int64, error) {
-	return parseLogsArg(service, logs, args)
-}
+var ParseLogsArg = parseLogsArg
