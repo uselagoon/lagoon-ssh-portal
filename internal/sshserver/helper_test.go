bc178ce2b87ce467f7f4fadb635c7d847f9aae13
Scott Leggett <scott@sl.id.au>
2024-12-13 10:31:11 +0800

    chore: add tests for sshserver authhandler

--- internal/sshserver/helper_test.go
+++ internal/sshserver/helper_test.go
@@ -1,17 +1,13 @@
 package sshserver
 
-// ParseConnectionParams exposes the private parseConnectionParams for testing
-// only.
-var ParseConnectionParams = parseConnectionParams
-
-// ParseLogsArg exposes the private parseLogsArg for testing only.
-var ParseLogsArg = parseLogsArg
-
-// SessionHandler exposes the private sessionHandler for testing only.
-var SessionHandler = sessionHandler
-
-// PermissionsMarshal exposes the private permissionsMarshal for testing only.
-var PermissionsMarshal = permissionsMarshal
+// These variables are exposed for testing only.
+var (
+	ParseConnectionParams = parseConnectionParams
+	ParseLogsArg          = parseLogsArg
+	PermissionsMarshal    = permissionsMarshal
+	SessionHandler        = sessionHandler
+	PubKeyHandler         = pubKeyHandler
+)
 
 // Exposes the private ctxKey constants for testing only.
 const (
